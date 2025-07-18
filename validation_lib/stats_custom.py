1ad42bc7006fd482cfa83b40bb3985041901cba1
Pitágoras Alves <alves.pitagoras@gmail.com>
2025-07-18 19:22:01 -0300

    New module: validation_lib Importing results from trained swarm and saving as a tool results dataframe Comparing multiple tools with same metrics Implemented fast Fmax calculation

---
+++ validation_lib/stats_custom.py
@@ -0,0 +1,132 @@
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+
+from sklearn import metrics
+from sklearn.metrics import precision_score, recall_score
+
+#SKLearn implementation of the Fmax metric
+def fast_fmax(pred_scores, truth_set, thresholds=None):
+    if thresholds is None:
+        thresholds = np.linspace(0, 1, 80)
+        print('thresholds:', thresholds)
+    best_fmax = 0.0
+    best_th = 0.0
+    for th in tqdm(thresholds):
+        pred_bin = (pred_scores > th).astype(int)
+        # Vetorizado: calcula média por proteína (sample average)
+        rec = recall_score(truth_set, pred_bin, average='samples', zero_division=0)
+        prec = precision_score(truth_set, pred_bin, average='samples', zero_division=0)
+        if rec + prec > 0:
+            f = 2 * prec * rec / (prec + rec)
+        else:
+            f = 0.0
+        if f > best_fmax:
+            best_fmax = f
+            best_th = th
+            print(best_fmax, best_th)
+    return best_fmax, best_th
+
+#Numpy implementation of the Fmax metric
+def faster_fmax(pred_scores, truth_set, thresholds=None):
+    if thresholds is None:
+        thresholds = np.linspace(0, 1, 80)
+    # pred_scores: (n_samples, n_labels)
+    # truth_set: (n_samples, n_labels)
+    n_samples, n_labels = pred_scores.shape
+    n_thresh = len(thresholds)
+    # Cria matriz booleana para todos os thresholds: shape (n_thresh, n_samples, n_labels)
+    pred_bin = (pred_scores[None, :, :] > thresholds[:, None, None]).astype(np.uint8)
+    truth = truth_set[None, :, :].astype(np.uint8)
+
+    # True positives, false positives, false negatives para cada threshold
+    tp = np.sum(pred_bin & truth, axis=2)  # shape (n_thresh, n_samples)
+    fp = np.sum(pred_bin & (~truth), axis=2)
+    fn = np.sum((~pred_bin) & truth, axis=2)
+
+    # Precisão e recall por amostra e threshold
+    with np.errstate(divide='ignore', invalid='ignore'):
+        prec = np.where(tp + fp > 0, tp / (tp + fp), 0)
+        rec = np.where(tp + fn > 0, tp / (tp + fn), 0)
+
+    # Média por amostra (samples), depois média por threshold
+    prec_mean = np.mean(prec, axis=1)
+    rec_mean = np.mean(rec, axis=1)
+
+    # F-score para cada threshold
+    with np.errstate(divide='ignore', invalid='ignore'):
+        f = np.where(prec_mean + rec_mean > 0, 2 * prec_mean * rec_mean / (prec_mean + rec_mean), 0)
+
+    best_idx = np.argmax(f)
+    best_fmax = f[best_idx]
+    best_th = thresholds[best_idx]
+    return float(best_fmax), float(best_th)
+
+def norm_with_baseline(metric_val, metric_baseline, max_value = 1.0):
+    actual_range = max_value - metric_baseline
+    actual_zero = metric_baseline
+    metric_norm = (metric_val - actual_zero) / actual_range
+    return metric_norm
+
+def calc_metrics_at_freq_threshold(col_sums, true_labels, val_y_pred, 
+        min_freq, labels_sequence, labels_path):
+    print('min freq:', min_freq)
+    valid_cols = col_sums > min_freq
+
+    true_labels_f = true_labels[:, valid_cols]
+    val_y_pred_f = val_y_pred[:, valid_cols]
+    baseline_pred_f = np.random.rand(*val_y_pred_f.shape)
+
+    #min_max_scaler = preprocessing.MinMaxScaler()
+    #val_y_pred_f_scaled = min_max_scaler.fit_transform(val_y_pred_f)
+    labels_sequence_f = [label for i, label in enumerate(labels_sequence) if valid_cols[i]]
+
+    print(f"Removed {np.sum(~valid_cols)} columns with zero frequency in true_labels.")
+
+    evaluated_label_sequence_path = labels_path.replace('.txt', f'.min_{min_freq}.evaluated.txt')
+    open(evaluated_label_sequence_path, 'w').write('\n'.join(labels_sequence_f))
+
+    print('Comparing')
+    fmax, bestrh = faster_fmax(val_y_pred_f, true_labels_f)
+    roc_auc_score_mac = metrics.roc_auc_score(true_labels_f, val_y_pred_f, average='macro')
+    roc_auc_score_mac_base = metrics.roc_auc_score(true_labels_f, baseline_pred_f, average='macro')
+    roc_auc_score_mac_norm = norm_with_baseline(roc_auc_score_mac, roc_auc_score_mac_base)
+    print(roc_auc_score_mac, roc_auc_score_mac_norm)
+    roc_auc_score_w = metrics.roc_auc_score(true_labels_f, val_y_pred_f, average='weighted')
+    roc_auc_score_w_base = metrics.roc_auc_score(true_labels_f, baseline_pred_f, average='weighted')
+    roc_auc_score_w_norm = norm_with_baseline(roc_auc_score_w, roc_auc_score_w_base)
+    print(roc_auc_score_w, roc_auc_score_w_norm)
+    auprc_mac = metrics.average_precision_score(true_labels_f, val_y_pred_f)
+    auprc_mac_base = metrics.average_precision_score(true_labels_f, baseline_pred_f)
+    auprc_mac_norm = norm_with_baseline(auprc_mac, auprc_mac_base)
+    print(auprc_mac, auprc_mac_norm)
+    auprc_w = metrics.average_precision_score(true_labels_f, val_y_pred_f, average='weighted')
+    auprc_w_base = metrics.average_precision_score(true_labels_f, baseline_pred_f, average='weighted')
+    auprc_w_norm = norm_with_baseline(auprc_w, auprc_w_base)
+    print(auprc_w, auprc_w_norm)
+    new_m = {
+        'raw':{
+            'ROC AUC': float(roc_auc_score_mac),
+            'ROC AUC W': float(roc_auc_score_w),
+            'AUPRC': float(auprc_mac),
+            'AUPRC W': float(auprc_w),
+            'Fmax': fmax,
+            'Best F1 Threshold': bestrh 
+        },
+        'norm': {
+            'ROC AUC': float(roc_auc_score_mac_norm),
+            'ROC AUC W': float(roc_auc_score_w_norm),
+            'AUPRC': float(auprc_mac_norm),
+            'AUPRC W': float(auprc_w_norm),
+            'bases': {
+                'ROC AUC': float(roc_auc_score_mac_base),
+                'ROC AUC W': float(roc_auc_score_w_base),
+                'AUPRC': float(auprc_mac_base),
+                'AUPRC W': float(auprc_w_base),
+            }
+        },
+        'N Proteins': len(true_labels_f),
+        'Tool Labels': len(valid_cols),
+        'Evaluated Labels': len(labels_sequence_f)
+    }
+    return new_m
---
+++ validation_lib/validate_others.py
@@ -0,0 +1,83 @@
+import gzip
+import json
+import polars as pl
+import numpy as np
+import sys
+from glob import glob
+from tqdm import tqdm
+import os
+
+from stats_custom import calc_metrics_at_freq_threshold
+
+def calc_metrics_for_tool(df_path, experimental_annots, max_n=300):
+    print(df_path)
+    print('Loading')
+    labels_path = df_path.replace('-preds.parquet', '-label_names.txt')
+    labels_sequence = open(labels_path, 'r').read().split('\n')
+    df = pl.read_parquet(df_path)
+    ids_list = df['id'].to_list()
+    true_labels = []
+    
+    print('Creating true labels')
+    for uniprot in ids_list:
+        true_vec = [1.0 if go in experimental_annots[uniprot] else 0.0 for go in labels_sequence]
+        true_labels.append(true_vec)
+    val_y_pred = df['labels'].to_list()
+    if len(true_labels) > max_n:
+        true_labels = np.asarray(true_labels)[:max_n]
+        val_y_pred = np.asarray([np.array(v) for v in val_y_pred])[:max_n]
+    else:
+        true_labels = np.asarray(true_labels)
+        val_y_pred = np.asarray([np.array(v) for v in val_y_pred])
+    print(val_y_pred)
+    col_sums = true_labels.sum(axis=0)
+    #freq_thresholds = [9, 7, 5, 3, 1, 0]
+    freq_thresholds = [5, 3, 0]
+    for min_freq in freq_thresholds:
+        print('min freq:', min_freq)
+        print(f"Removed {np.sum(~(col_sums > min_freq))} columns with low frequency in true_labels.")
+    metrics_dict = {}
+    
+    for min_freq in freq_thresholds:
+        new_m = calc_metrics_at_freq_threshold(col_sums, true_labels, val_y_pred, 
+            min_freq, labels_sequence, labels_path)
+        metrics_dict[f'Min_Freq_{min_freq+1}'] = new_m
+        print(json.dumps(new_m, indent=2))
+    print(metrics_dict)
+    return metrics_dict
+
+#sys.argv = ['others/validate_others.py', '~/data/protein_dimension_db/release_1/', 
+#    '40', '0.15', '~/data/mf_swarm_datasets/other_tools/']
+prot_dimension_db_release_path = os.path.expanduser(sys.argv[1])
+min_protein_annots = sys.argv[2]
+val_part = sys.argv[3]
+others_dir = os.path.expanduser(sys.argv[4])
+
+print('Loading validation proteins')
+validation_split_dir = prot_dimension_db_release_path + f'/validation_splits/min_prot{min_protein_annots}_val{val_part}'
+val_ids = validation_split_dir+'/validation_ids.txt'
+validation_proteins = open(val_ids, 'r').read().split('\n')
+
+print('Loading experimental annotations')
+mf_annotations_path = f'{prot_dimension_db_release_path}/go.experimental.mf.tsv.gz'
+annots_cache = f'{validation_split_dir}/experimental_annots.json'
+if os.path.exists(annots_cache):
+    experimental_annots = json.load(open(annots_cache, 'r'))
+else:
+    experimental_annots = {}
+    bar1 = tqdm(total=len(validation_proteins))
+    for rawline in gzip.open(mf_annotations_path, 'rt').readlines():
+        uniprot_id, labels = rawline.rstrip('\n').split('\t')
+        if uniprot_id in validation_proteins:
+            experimental_annots[uniprot_id] = labels.split(',')
+            bar1.update(1)
+    bar1.close()
+
+    json.dump(experimental_annots, open(annots_cache, 'w'), indent=4)
+
+print('Analyzing tools')
+df_paths = glob(others_dir+'/*-preds.parquet')
+metrics_all = {}
+for df_path in tqdm(reversed(df_paths)):
+    metrics_all[df_path] = calc_metrics_for_tool(df_path, experimental_annots)
+json.dump(metrics_all, open(f'{others_dir}/validation_results.json', 'w'), indent=4)
